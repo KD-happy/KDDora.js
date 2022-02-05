743a9ab5427c11714c31e4995104304f7c3d5e09
KD-happy <2507086134@qq.com>
2022-02-05 17:49:49 +0800

    修复只能看一页Bug

--- AcFun/components/up_list.js
+++ AcFun/components/up_list.js
@@ -29,10 +29,40 @@ async function getUp(u) {
     return data;
 }
 
+async function getUp(u, page) {
+    var url = `https://www.acfun.cn/u/${u}?quickViewId=ac-space-video-list&ajaxpipe=1&type=video&order=newest&page=${page}&pageSize=20`;
+    var res = await axios.get(url, {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+        }
+    })
+    var json = /(.*)\/\*/g.exec(res.data)[1];
+    var $ = cheerio.load(JSON.parse(json).html);
+    var space_video = $('.ac-space-video');
+    var data = [];
+    for (var i=0; i<space_video.length; i++) {
+        var eq = space_video.eq(i);
+        var ac = JSON.parse(eq.attr('data-wbinfo')).mediaId;
+        var img_url = $('.video img', eq).attr('src');
+        var title = $('.title.line', eq).text();
+        var play_info = $('.play-info', eq).text();
+        var date = $('.date', eq).text();
+        data.push({
+            ac: ac,
+            img_url: img_url,
+            title: title,
+            play_info: play_info,
+            date: date
+        })
+    }
+    return data;
+}
+
 module.exports = {
     type: 'list',
-    async fetch({args}) {
-        var list = await getUp(args.up);
+    async fetch({args, page}) {
+        page = page || 1;
+        var list = await getUp(args.up, page);
         var data = list.map(m => {
             return {
                 style: 'live',
@@ -43,6 +73,13 @@ module.exports = {
                 })
             }
         })
-        return data;
+        if (data.length < 20) {
+            return data;
+        } else {
+            return {
+                nextPage: page+1,
+                items: data
+            }
+        }
     }
 }
